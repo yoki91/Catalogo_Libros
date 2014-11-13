2ecba5e71aa1b4b17c347aea8e574248106c8d82
yifei ge <yokihappy@hotmail.com>
2014-11-13 13:54:24 +0100

    Get Libro id

--- Catalogo-api/src/main/java/edu/upc/eetac/dsa/yifeige/Catalogo_api/LibroResource.java
+++ Catalogo-api/src/main/java/edu/upc/eetac/dsa/yifeige/Catalogo_api/LibroResource.java
@@ -1,5 +1,7 @@
 package edu.upc.eetac.dsa.yifeige.Catalogo_api;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
 import javax.ws.rs.*;
@@ -14,16 +16,20 @@ public class LibroResource
 	private DataSource ds = DataSourceSPA.getInstance().getDataSource();
 	
 	
-	private String Get_Libro_Query="select * from libros";
+	private String Get_Libro_Query="select * from libros where titulo LIKE ?";
+	@Path("/search")
 	@GET
 	@Produces(MediaType.Catalogo_API_LIBRO_COLLECTION)
-	public libro_Collection getlibros()
+	public libro_Collection getlibros(@QueryParam("titulo") String titulo)
 	{
 		libro_Collection libros =new libro_Collection();
 		Connection conn = null;
-		try {
+		try 
+		{
 			conn = ds.getConnection();
-		} catch (SQLException e) {
+		} 
+		catch (SQLException e) 
+		{
 			throw new ServerErrorException("Could not connect to the database",Response.Status.SERVICE_UNAVAILABLE);
 		}
 
@@ -31,6 +37,7 @@ public class LibroResource
 		try
 		{
 			stmt = conn.prepareStatement(Get_Libro_Query);
+			stmt.setString(1, '%'+titulo+'%');
 			ResultSet rs = stmt.executeQuery();
 			while(rs.next())
 			{
@@ -111,12 +118,16 @@ public class LibroResource
 		{
 			throw new ServerErrorException(e.getMessage(),Response.Status.INTERNAL_SERVER_ERROR);
 		}
-		finally {
-			try {
+		finally 
+		{
+			try 
+			{
 				if (stmt != null)
 					stmt.close();
 				conn.close();
-			} catch (SQLException e) {
+			} 
+			catch (SQLException e) 
+			{
 
 			}
 		}
@@ -126,61 +137,147 @@ public class LibroResource
 	
 	
 	
-	private String Get_Libro_By_Id_Query="select * from libros where libroid=?";
 	
-	private Libro ObtenerLibroDB(String libroid)
-	{
-		Libro libro = new Libro();
-		Connection conn = null;
-		try {
-			conn = ds.getConnection();
-		} catch (SQLException e) 
-		{
-			throw new ServerErrorException("Could not connect to the database",
-					Response.Status.SERVICE_UNAVAILABLE);
-		}
-
-		PreparedStatement stmt = null;
-		try
-		{
-			stmt = conn.prepareStatement(Get_Libro_By_Id_Query);
-			stmt.setInt(1, Integer.valueOf(libroid));
-			ResultSet rs = stmt.executeQuery();
-			
-			if(rs.next())
-			{
-				libro.setLibroid(rs.getInt("libroid"));
-				libro.setTitulo(rs.getString("titulo"));
-				libro.setLengua(rs.getString("lengua"));
-				libro.setEdicion(rs.getString("edicion"));
-				libro.setFecha_Edicion(rs.getString("Fecha_Edicion"));
-				libro.setFecha_Impresion(rs.getString("Fecha_Impresion"));
-				libro.setEditorial(rs.getString("Editorial"));
-			}
-			else 
-			{
-				throw new NotFoundException("There's no sting with libroid="+ libroid);
-			}
-			
-		}
-		catch (SQLException e) {
-			throw new ServerErrorException(e.getMessage(),
-					Response.Status.INTERNAL_SERVER_ERROR);
-		} finally 
-		{
+	   
+	
+	   @GET
+	   @Path("/{libroid}")
+	   @Produces(MediaType.Catalogo_API_LIBRO)
+	   public Response getlibro(@PathParam("libroid") String libroid,@Context Request request)
+	   {
+		   System.out.println("GetLibro");
+		   CacheControl cc = new CacheControl();
+		   Libro libro =ObtenerLibroDB(libroid);
+		   EntityTag eTag = new EntityTag(Long.toString(libro.getLastModified()));
+		   Response.ResponseBuilder rb=request.evaluatePreconditions(eTag);
+		   if(rb!=null)
+		   {
+			   return rb.cacheControl(cc).tag(eTag).build();
+		   }
+		   rb = Response.ok(libro).cacheControl(cc).tag(eTag);
+		   return rb.build();
+		   
+	   }
+	   
+	   
+	   
+	   
+	   private String Get_Libro_By_Id_Query="select * from libros where libroid=?";
+	   private String Get_autor_By_IdLibro_Query="select autores.nombre from libros,libro_autores,autores where libros.libroid=libro_autores.libroid and libro_autores.libroid=1 and autores.idautor=libro_autores.idautor";
+	   
+	   
+	   
+	   private Libro ObtenerautorDB(String libroid)
+	   {
+		   System.out.println("ObtenerAutorDB");
+		   Libro libro = new Libro();
+			Connection conn = null;
 			try {
-				if (stmt != null)
-					stmt.close();
-				conn.close();
+				conn = ds.getConnection();
 			} catch (SQLException e) 
 			{
-
+				throw new ServerErrorException("Could not connect to the database",
+						Response.Status.SERVICE_UNAVAILABLE);
 			}
-		}
-		return libro;
-	  
+
+			PreparedStatement stmt = null;
+			try
+			{
+				stmt = conn.prepareStatement(Get_autor_By_IdLibro_Query);
+				//stmt.setInt(1, Integer.valueOf(libroid));
+				
+				ResultSet rs = stmt.executeQuery();
+				System.out.println("result set: "+ rs.getString("nombre"));
+				List<Author> autores= new ArrayList<Author>();
+				if(rs.next())
+				{
+					
+					Author autor= new Author();
+					autor.setNombre(rs.getString("nombre"));
+					autor.setIdautor(rs.getInt("idautor"));
+					autores.add(autor);
+					libro.setAutores(autores);
+					System.out.println("Autor metido al libro");
+				}
+			}
+			catch (SQLException e) 
+			{
+				throw new ServerErrorException(e.getMessage(),Response.Status.INTERNAL_SERVER_ERROR);
+				
+			} finally {
+				try {
+					if (stmt != null)
+						stmt.close();
+					conn.close();
+				} catch (SQLException e) {
+
+				}
+			}
+			return libro;
+		   
 	   }
-	
+	   
+	   
+	   
+	   
+	   
+		private Libro ObtenerLibroDB(String libroid)
+		{
+			System.out.println("ObtenerLibroDB");
+			Libro libro = new Libro();
+			libro=ObtenerautorDB(libroid);
+			
+			Connection conn = null;
+			try {
+				conn = ds.getConnection();
+			} catch (SQLException e) 
+			{
+				throw new ServerErrorException("Could not connect to the database",
+						Response.Status.SERVICE_UNAVAILABLE);
+			}
+
+			PreparedStatement stmt = null;
+			try
+			{
+				stmt = conn.prepareStatement(Get_Libro_By_Id_Query);
+				stmt.setInt(1, Integer.valueOf(libroid));
+				ResultSet rs = stmt.executeQuery();
+				
+				if(rs.next())
+				{
+					libro.setLibroid(rs.getInt("libroid"));
+					libro.setTitulo(rs.getString("titulo"));
+					libro.setLengua(rs.getString("lengua"));
+					libro.setEdicion(rs.getString("edicion"));
+					libro.setFecha_Edicion(rs.getString("Fecha_Edicion"));
+					libro.setFecha_Impresion(rs.getString("Fecha_Impresion"));
+					libro.setEditorial(rs.getString("Editorial"));
+					libro.setLastModified(rs.getTimestamp("last_modified").getTime());
+				}
+				else 
+				{
+					throw new NotFoundException("There's no sting with libroid="+ libroid);
+				}
+				
+			}
+			catch (SQLException e) {
+				throw new ServerErrorException(e.getMessage(),
+						Response.Status.INTERNAL_SERVER_ERROR);
+			} finally 
+			{
+				try {
+					if (stmt != null)
+						stmt.close();
+					conn.close();
+				} catch (SQLException e) 
+				{
+
+				}
+			}
+			return libro;
+		  
+		   }
+	   
 	
 	
 	
